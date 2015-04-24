f47266a9b877d941eb339958601dd24ce89e1bef
Nakib Momin <nbmomin14@gmail.com>
2015-04-24 17:04:04 -0400

    added code to add item

--- ToDo/SecondViewController.swift
+++ ToDo/SecondViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var todoItem: UITextField!
+    
     override func viewDidLoad() {
+        self.todoItem.delegate = self
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
 
+    @IBAction func addItem(sender: UIButton) {
+        if(!todoItem.text.isEmpty){
+            //add it to the view
+        }
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        todoItem.resignFirstResponder()
+    }
 
 }
 
