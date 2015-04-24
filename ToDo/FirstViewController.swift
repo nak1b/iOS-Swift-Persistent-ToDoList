87367a99936858a1a81572c371a10924f6097c78
Nakib Momin <nbmomin14@gmail.com>
2015-04-24 17:12:14 -0400

    Added Table view and ability to store data with arrays

--- ToDo/FirstViewController.swift
+++ ToDo/FirstViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = ["Item Number One", "Another Sample Item", "One More"]
+
+class FirstViewController: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return toDoList.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoList[indexPath.row]
+        return cell
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
